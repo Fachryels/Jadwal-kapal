a7ed372599c08e97c14ed489dcb9af0962e1ffb6
Fachryels <66047873+Fachryels@users.noreply.github.com>
2025-09-19 10:33:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTN5FOSUM77lPnkjReqiyTlM5paNh_VMPu27TuQJxip6n7pD7su15lVqyBqwvJ-TKyp7AXJGuT26l11/pub?gid=0&single=true&output=csv';
 const BANNER_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTN5FOSUM77lPnkjReqiyTlM5paNh_VMPu27TuQJxip6n7pD7su15lVqyBqwvJ-TKyp7AXJGuT26l11/pub?gid=31431312&single=true&output=csv';
+const ANCHORAGE_URL = 'anchorage.csv';
 
 async function fetchData() {
   const response = await fetch(CSV_URL);
   const data = await response.text();
   const rows = data.split('\n').slice(1); // skip header
   const tbody = document.querySelector('#jadwal tbody');
+  tbody.innerHTML = '';
+  rows.forEach(r => {
+    if (r.trim() === '') return;
+    const cols = r.split(',');
+    const tr = document.createElement('tr');
+    cols.forEach(c => {
+      const td = document.createElement('td');
+      td.textContent = c;
+      tr.appendChild(td);
+    });
+    tbody.appendChild(tr);
+  });
+}
+
+
+// Ambil data kapal sedang Anchorage
+async function fetchAnchorage() {
+  const response = await fetch(ANCHORAGE_URL);
+  const data = await response.text();
+  const rows = data.split('\n').slice(1); // skip header
+  const tbody = document.querySelector('#anchorage tbody');
   tbody.innerHTML = '';
   rows.forEach(r => {
     if (r.trim() === '') return;
